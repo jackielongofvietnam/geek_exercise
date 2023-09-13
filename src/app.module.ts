59e74f6652a81987a8c246ea92b2d45090fff5dd
jackielongofvietnam <jackielongofvietnam@gmail.com>
2023-09-13 13:31:53 +0700

    start

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './users/controller/users/users.controller';
-import { ControllerController } from './users/controller/controller.controller';
+
 
 @Module({
   imports: [],
-  controllers: [AppController, UsersController, ControllerController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
